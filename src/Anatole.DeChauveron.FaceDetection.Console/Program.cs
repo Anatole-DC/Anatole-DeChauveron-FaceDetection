a2659047f0f32ba1707af65e4468701cb012645c
Anatole-DC <adechauveron@gmail.com>
2023-01-17 11:27:08 +0100

    :tada: Initial commit

---
+++ src/Anatole.DeChauveron.FaceDetection.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json;
+using Anatole.DeChauveron.FaceDetection;
+
+namespace Anatole.DeChauveron.FaceDetection.Console;
+
+
+class Program
+{
+    static void Main(String[] args)
+    {
+
+        IList<byte[]> images = new List<byte[]>();
+        foreach (string argument in args)
+        {
+            images.Add(System.IO.File.ReadAllBytes(argument));
+        }
+
+        IList<FaceDetectionResult> detectFaceInScenesResults = new FaceDetection().DetectInScenes(images);
+    
+        foreach (var detectionResult in detectFaceInScenesResults)
+        {
+            System.Console.WriteLine($"Points:{JsonSerializer.Serialize(detectionResult.Points)}");
+        } 
+    }
+}
+
---
+++ src/Anatole.DeChauveron.FaceDetection/FaceDetectionResult.cs
@@ -0,0 +1,12 @@
+namespace Anatole.DeChauveron.FaceDetection;
+
+public record FaceDetectionResult
+{
+    public byte[] ImageData { get; set; }
+    public IList<ObjectDetectionPoint> Points { get; set; }
+}
+public record ObjectDetectionPoint
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+}
